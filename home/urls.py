3b184277b9b093a91bed282fc55be4f7efa45282
Shakthidhar Vilvanathan <Shakthi91124@users.noreply.github.com>
2020-09-02 17:28:22 +0530

    Search functionality ready

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('new_search', views.new_search, name='new_search'),
+    path('google_search', views.google_search, name='google_search'),
+    path('wiki_search', views.wiki_search, name='wiki_search'),
+    path('ytube_search', views.ytube_search, name='ytube_search'),
+    path('gmaps_search', views.gmaps_search, name='gmaps_search'),
+    path('google_response', views.google_response, name='google_response'),
+    path('wiki_response', views.wiki_response, name='wiki_response'),
+    path('ytube_response', views.ytube_response, name='ytube_response'),
+    path('gmaps_response', views.gmaps_response, name='gmaps_response'),
 ]
--- home/views.py
+++ home/views.py
@@ -20,14 +20,95 @@ def new_search(request):
     search = request.POST.get('search')
     if 'good morning cookie' in search:
         response = 'good morning'
-        print(response)
-    return render(request, 'home/new_search.html')
+    elif 'good afternoon cookie' in search:
+        response = 'good afternoon'
+    elif 'how are you doing cookie' in search:
+        response = 'Pretty Well'
+    elif 'how are you cookie' in search:
+        response = 'I am fine how about you'
+    elif 'what is your name' in search:
+        response = 'My name is Cookie'
+    elif 'who are you' in search:
+        response = 'My name is Cookie. The Virtual Assistant. Version 1.0'
+    elif 'tell me about yourself' in search:
+        response = 'I am Cookie. The Virtual Assistant. Version 1.0'
+    elif 'hey cookie what is the time' in search:
+        response = ctime()
+    elif 'what is the time cookie' in search:
+        response = ctime()
+    elif 'what is the time' in search:
+        response = ctime()
+    elif 'hey cookie what time is it' in search:
+        response = ctime()
+    elif 'what time is it cookie' in search:
+        response = ctime()
+    elif 'tell me the time cookie' in search:
+        response = ctime()
+    elif 'hey cookie tell me the time' in search:
+        response = ctime()
+    else:
+        response = "Sorry I can't understand you!!.."
+
+    context = {
+        'search': search,
+        'response': response
+    }
+    return render(request, 'home/new_search.html', context)
 
 
-def respond(voice_data):
-    if 'good morning cookie' in voice_data:
-        response = 'good morning'
-        return response
+def google_search(request):
+    return render(request, 'home/google_search.html')
+
+
+def wiki_search(request):
+    return render(request, 'home/wiki_search.html')
+
+
+def ytube_search(request):
+    return render(request, 'home/ytube_search.html')
+
+
+def gmaps_search(request):
+    return render(request, 'home/gmaps_search.html')
+
+
+def google_response(request):
+    search = request.POST.get('search')
+    url = 'https://google.com/search?q=' + search
+    webbrowser.get().open(url)
+    context = {
+        'response': 'Your search results will appear in new tab!!..'
+    }
+    return render(request, 'home/google_response.html', context)
+
+
+def wiki_response(request):
+    search = request.POST.get('search')
+    url = 'https://en.wikipedia.org/wiki/Special:Search?search=' + search
+    webbrowser.get().open(url)
+    context = {
+        'response': 'Your search results will appear in new tab!!..'
+    }
+    return render(request, 'home/wiki_response.html', context)
+
+
+def ytube_response(request):
+    search = request.POST.get('search')
+    kit.playonyt(search)
+    context = {
+        'response': 'Your search results will appear in new tab!!..'
+    }
+    return render(request, 'home/ytube_response.html', context)
+
+
+def gmaps_response(request):
+    search = request.POST.get('search')
+    url = 'https://google.nl/maps/place/' + search + '/&amp;'
+    webbrowser.get().open(url)
+    context = {
+        'response': 'Your search results will appear in new tab!!..'
+    }
+    return render(request, 'home/gmaps_response.html', context)
 
 
 def listen_audio(ask=False):
